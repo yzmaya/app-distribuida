853c97afe303e118230b61eda3341de9a2828476
Néstor Yzmaya <nestor.tarinda@gmail.com>
2019-04-23 14:08:40 -0500

    mod index y app.js
    Se quito el inicio de sesión con correo electrónico y se cambio source-src por worker-src, ya que a 23 de abril de 2019 es vulnerable dicha sentencia

--- js/app.js
+++ js/app.js
@@ -64,45 +64,7 @@ providerFb.addScope('public_profile');
 
 //************************aqui termina el inicio de sesión con Facebook ****************************
 
-  // Obtener elementos
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-
-  const error = document.getElementById('demo');
-  const prevencion = document.getElementById('demo2');
-
-    // Añadir Evento login
-  btnLogin.addEventListener('click', e => {
-    //Obtener email y pass
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    // Sign in
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message)); 
-    promise.catch(e => error.innerHTML = e.message); 
-     
-  });
-
-  // Añadir evento signup
-  btnSignUp.addEventListener('click', e => {
-    // Obtener email y pass
-    // TODO: comprobar que el email sea real
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    // Sign in
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-    promise.catch(e => error.innerHTML = e.message); 
-  });
-
-
-
+  
 
 
 // Añadir un listener en tiempo real
